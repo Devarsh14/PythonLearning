12574d41e36d1ecf6864abd9042e0352dba82573
DevarshShukla <devarsh14@gmail.clom>
2024-09-02 09:39:43 +1000

    Added virtual env

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -1,1 +1,3 @@
 print("Hello, World!")
+
+# To take snapshot of installed packages. pip freeze > requirements.txt
---
+++ FastApiLearning/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# To run python main.py
+# uvicorn main:app --reload
